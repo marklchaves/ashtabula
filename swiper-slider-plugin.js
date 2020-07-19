6823a9fbf0d254fc9b41ab4c7e059f98eae149fd
mark l chaves <mark@caughtmyeye.cc>
2020-07-20 06:38:11 +0800

    Convert to IIFE

--- swiper-slider-plugin.js
+++ swiper-slider-plugin.js
@@ -1,9 +1,11 @@
-var swiper = new Swiper(".swiper-container", {
-  autoplay: {
-    delay: 5000,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
+(function () {
+  let swiper = new Swiper(".swiper-container", {
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+})();
